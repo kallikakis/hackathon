2b7207e8dc1ea95c71b1aacdc77d7e7c0282de8d
Claudiu-Mihai Arba <Claudiu.Mihai.Arba@doclerholding.com>
2017-03-11 21:32:32 +0100

    Implemented detail service.

--- undefined/src/main/java/com/doclerholding/hackaton/controller/ParkingSpotsController.java
+++ undefined/src/main/java/com/doclerholding/hackaton/controller/ParkingSpotsController.java
@@ -1,28 +1,25 @@
 package com.doclerholding.hackaton.controller;
 
 import com.doclerholding.hackaton.data.model.parking.ParkSpotDetail;
+import com.doclerholding.hackaton.service.DetailsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by claudiu.arba on 11/03/17.
  */
 @Controller
 @RequestMapping("/parking/detail")
-public class ParkingSpotsController extends PublicDataDetailedController<ParkSpotDetail> {
+public class ParkingSpotsController {
 
-	public ParkingSpotsController() {
-		super(ParkSpotDetail.class);
-	}
+	@Autowired
+	DetailsService detailsService;
 
-	@Override
+	@RequestMapping("/{id}")
 	protected ParkSpotDetail getDetailsById(@PathVariable String id) throws JsonProcessingException {
-		return getDetailedData(id, "Occupancy/CarPark/{id}");
+		return detailsService.getParkingDetail(id);
 	}
 }
