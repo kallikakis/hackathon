2488fca6cd96eadee72562e9309a0975e937c512
maxim <maxim.badarau@gmail.com>
2017-03-11 16:08:06 +0100

    Added AbstractLoader

--- undefined/src/main/java/com/doclerholding/hackaton/data/loaders/AirQualityLineLoader.java
+++ undefined/src/main/java/com/doclerholding/hackaton/data/loaders/AirQualityLineLoader.java
@@ -1,54 +1,29 @@
 package com.doclerholding.hackaton.data.loaders;
 
-import com.doclerholding.hackaton.data.model.AirQualityModel;
-import com.doclerholding.hackaton.data.model.Poi;
-import com.doclerholding.hackaton.data.repository.PoiRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by maxim on 3/11/2017.
  */
 @Component
-public class AirQualityLineLoader {
+public class AirQualityLineLoader extends AbstractLineLoader {
 
 	private static Logger logger = LoggerFactory.getLogger(AirQualityLineLoader.class);
-
-	@Autowired
-	private PoiRepository poiRepository;
 
 	@PostConstruct
 	public void loadAirQualityLine() throws URISyntaxException, IOException {
 		final ObjectMapper mapper = new ObjectMapper();
 		JsonNode rootNode = mapper.readTree(new File(Thread.currentThread().getContextClassLoader().getResource("air_quality.json").toURI()));
-		List<Poi> data = new ArrayList<>();
-		JsonNode features = rootNode.path("features");
-		for (JsonNode f : features) {
-			AirQualityModel airQualityModel = new AirQualityModel();
-			airQualityModel.setType("air_quality");
-			JsonNode geometry = f.path("geometry");
-			if (geometry.path("type").asText().equals("Point")) {
-				JsonNode coordinates = geometry.path("coordinates");
-				airQualityModel.setLocation(new GeoPoint(coordinates.path(0).asDouble(), coordinates.path(1).asDouble()));
-			}
-			JsonNode props = f.path("properties");
-			airQualityModel.setId(props.path("id").asText());
-			airQualityModel.setName(props.path("name").asText());
-			data.add(airQualityModel);
-			//this.poiRepository.index(airQualityModel);
-		}
+		addPoint(rootNode, "air_quality");
 	}
 
 }
